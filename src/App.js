2924f5d3cc8a79f2065c4d11564ece1be02f2912
leonardobussi <leonardobezerrabussy@gmail.com>
2021-06-22 22:57:06 -0300

    teste do uso do autocomplete do material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react"
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch("https://streinvestapi.herokuapp.com/consortium/fjuhrHbtkje3qUoEqWzo")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Autocomplete
+        id="combo-box-demo"
+        options={data.response}
+        getOptionLabel={(option) => option.consortium_name}
+        style={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="consorcios" variant="outlined" />}
+      />
     </div>
   );
 }
